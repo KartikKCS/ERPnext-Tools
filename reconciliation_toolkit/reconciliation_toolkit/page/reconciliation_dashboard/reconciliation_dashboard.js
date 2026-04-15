90141618eaf6b3e159c611166b894cd679c38e5d
GAUTAMJHALARIA <jhalariagautam@gmail.com>
2026-04-15 09:06:14 +0530

    rearrange the squence of tab and added abrreveations

--- reconciliation_toolkit/reconciliation_toolkit/page/reconciliation_dashboard/reconciliation_dashboard.js
+++ reconciliation_toolkit/reconciliation_toolkit/page/reconciliation_dashboard/reconciliation_dashboard.js
@@ -736,14 +736,18 @@ function mount_vue_app() {
                 <button v-if="!sidebarOpen" class="btn btn-xs btn-default" @click="sidebarOpen = true" style="margin-right: 8px;">☰</button>
                 <button v-if="sel_booking" class="btn btn-xs btn-default" @click="goBack">← All Bookings</button>
                 <template v-if="!sel_booking">
+                    <button class="btn btn-xs rc-tab-btn" :class="view==='folios' ? 'btn-primary':'btn-default'"
+                            @click="setView('folios')">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="14" height="14" fill="currentColor" style="vertical-align:-2px;margin-right:3px;"><path d="M14 2H6c-1.1 0-2 .9-2 2v16c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V8l-6-6zm4 18H6V4h7v5h5v11zM8 15.01l1.41 1.41L11 14.84V19h2v-4.16l1.59 1.59L16 15.01 12.01 11 8 15.01z"/></svg>
+                        Res.</button>
                     <button class="btn btn-xs rc-tab-btn" :class="view==='group_bookings' ? 'btn-primary':'btn-default'"
                             @click="setView('group_bookings')" :disabled="!has_bk">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="14" height="14" fill="currentColor" style="vertical-align:-2px;margin-right:3px;"><path d="M16 11c1.66 0 2.99-1.34 2.99-3S17.66 5 16 5c-1.66 0-3 1.34-3 3s1.34 3 3 3zm-8 0c1.66 0 2.99-1.34 2.99-3S9.66 5 8 5C6.34 5 5 6.34 5 8s1.34 3 3 3zm0 2c-2.33 0-7 1.17-7 3.5V19h14v-2.5c0-2.33-4.67-3.5-7-3.5zm8 0c-.29 0-.62.02-.97.05 1.16.84 1.97 1.97 1.97 3.45V19h6v-2.5c0-2.33-4.67-3.5-7-3.5z"/></svg>
-                        Group</button>
+                        Grp.</button>
                     <button class="btn btn-xs rc-tab-btn" :class="view==='individual_bookings' ? 'btn-primary':'btn-default'"
                             @click="setView('individual_bookings')" :disabled="!has_bk">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="14" height="14" fill="currentColor" style="vertical-align:-2px;margin-right:3px;"><path d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z"/></svg>
-                        Individual</button>
+                        Ind.</button>
                     <button class="btn btn-xs rc-tab-btn" :class="view==='tpa_bookings' ? 'btn-primary':'btn-default'"
                             @click="setView('tpa_bookings')" :disabled="!has_bk">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="14" height="14" fill="currentColor" style="vertical-align:-2px;margin-right:3px;"><path d="M21 16v-2l-8-5V3.5c0-.83-.67-1.5-1.5-1.5S10 2.67 10 3.5V9l-8 5v2l8-2.5V19l-2 1.5V22l3.5-1 3.5 1v-1.5L13 19v-5.5l8 2.5z"/></svg>
@@ -751,11 +755,7 @@ function mount_vue_app() {
                     <button class="btn btn-xs rc-tab-btn" :class="view==='company_bookings' ? 'btn-primary':'btn-default'"
                             @click="setView('company_bookings')" :disabled="!has_bk">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="14" height="14" fill="currentColor" style="vertical-align:-2px;margin-right:3px;"><path d="M12 7V3H2v18h20V7H12zM6 19H4v-2h2v2zm0-4H4v-2h2v2zm0-4H4V9h2v2zm0-4H4V5h2v2zm4 12H8v-2h2v2zm0-4H8v-2h2v2zm0-4H8V9h2v2zm0-4H8V5h2v2zm10 12h-8v-2h2v-2h-2v-2h2v-2h-2V9h8v10zm-2-8h-2v2h2v-2zm0 4h-2v2h2v-2z"/></svg>
-                        Company</button>
-                    <button class="btn btn-xs rc-tab-btn" :class="view==='folios' ? 'btn-primary':'btn-default'"
-                            @click="setView('folios')">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="14" height="14" fill="currentColor" style="vertical-align:-2px;margin-right:3px;"><path d="M14 2H6c-1.1 0-2 .9-2 2v16c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V8l-6-6zm4 18H6V4h7v5h5v11zM8 15.01l1.41 1.41L11 14.84V19h2v-4.16l1.59 1.59L16 15.01 12.01 11 8 15.01z"/></svg>
-                        Folios</button>
+                        Corp.</button>
                 </template>
                 <span v-if="sel_booking" class="rc-crumb" style="margin-left: 8px;">{{ sel_booking }}</span>
             </div>
