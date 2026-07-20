23f58eea56f8998c27a130d4852b04a30ff463bf
Kartik <k.vishwakarma@katalystcs.com.au>
2026-07-20 09:54:31 +0530

    chore: Clean up scratch scripts, mock json and Zone.Identifiers

--- reconciliation_toolkit/my_test2.py
+++
@@ -1,8 +0,0 @@
-from reconciliation_toolkit.reconciliation_toolkit.services.purchase_recon_engine import run_purchase_reconciliation
-import json
-
-def run_test():
-    res = run_purchase_reconciliation('2025-01-01', '2027-01-01')
-    sc = res['summary']['reconciliation'].get('supplier_scorecards')
-    with open('/tmp/test_scorecard.json', 'w') as f:
-        json.dump(sc, f)
